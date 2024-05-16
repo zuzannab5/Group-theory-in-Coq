74b2cd8d68b1666a5840c2408325a684593f1fe8
zuzannab5 <56296468+zuzannab5@users.noreply.github.com>
2024-05-16 18:49:07 +0200

    Dodanie tw o jednoznaczności elementu neutralnego

--- main.v
+++ main.v
@@ -9,3 +9,19 @@ Record GroupTheo : Type := groupTheo
     inv := forall(x : G), exists( e y : G) , op x y = e /\ op y x  = e; 
   }.
 
+Definition idPro (g : GroupTheo ) (e : G g) := (forall(x : G g), op g e x = x /\ op g x e = x).
+
+
+Theorem exOnlyOne : forall (g : GroupTheo), forall( e f : G g), (idPro g e /\ idPro g f) -> e = f.
+
+Proof.  
+  unfold idPro.
+  intros.
+  destruct H.
+  pose proof H f as F.
+  pose proof H0 e as E.
+  destruct E.
+  destruct F.
+  rewrite <- H2.
+  trivial.
+Qed.
