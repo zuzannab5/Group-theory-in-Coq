e519339b87d50499bd031cd8d4d034a965b550ff
zuzannab5 <56296468+zuzannab5@users.noreply.github.com>
2024-05-21 10:46:49 +0200

    Dodanie dowodu pPowerGivesAbelian + zmiany w strukturze poprzednich dowodów

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # Group-theory-in-Coq
 1. Podstawowe definicje
-   - [x] Definicja typu grupy i 
-   - [ ] Definicja podgrupy, 
+   - [x] Definicja typu grupy `GroupTheo` i `Group`
+   - [ ] Definicja podgrupy
    - [ ] *definicja warstwy lewostronnej/prawo*
 
 2. Proste twierdzenia dla grup i przykłady grupy i podgrupy
 
-	- [x] a.  istnieje dokładnie jeden el neutralny
+	- [x] a.  istnieje dokładnie jeden el neutralny `exOnlyOne`
 
-	- [x] b. dla dowolnego el istnieje dokładnie 1 el odwrotny
+	- [x] b. dla dowolnego el istnieje dokładnie 1 el odwrotny `exOnlyOneInv`
 
 	- [ ] c. prawo skracania z lewej i prawej
 
@@ -22,6 +22,6 @@
 	- [ ] pokazać, że Z2 to podgrupa Z4
 
 3. Grupy abelowe
-    - [ ] a. definicja grupy abelowej
+    - [x] a. definicja grupy abelowej `AbelianGroup`
     - [ ] b. przykład, że Z3 jest abelowa.
-    - [ ] c. udowodnić, że grupa, w której każdy element spełnia warunek x^2 = e jest abelowa
+    - [x] c. udowodnić, że grupa, w której każdy element spełnia warunek x^2 = e jest abelowa `pPowerGivesAbelian`
--- main.v
+++ main.v
@@ -1,5 +1,5 @@
 Require Import Setoid.
-
+Require Import Lia.
 
 Record GroupTheo : Type := groupTheo
   { Gt : Set; (* nosnik *)
@@ -9,46 +9,51 @@ Record GroupTheo : Type := groupTheo
     invt := forall(x : Gt), exists( e y : Gt) , opt x y = e /\ opt y x  = e; 
   }.
 
-(* Jednoznaczność elementu neutralnego *)
+
+
+(* Jednoznaczność elementu neutralnego - krótszy zapis*)
 Definition idPro (g : GroupTheo ) (e : Gt g) := (forall(x : Gt g), opt g e x = x /\ opt g x e = x).
+
 
 Theorem exOnlyOne : forall (g : GroupTheo), forall( e f : Gt g), (idPro g e /\ idPro g f) -> e = f.
 Proof.  
   unfold idPro.
   intros.
   destruct H.
-  pose proof H f as F.
-  pose proof H0 e as E.
-  destruct E.
-  destruct F.
-  rewrite <- H2.
+  specialize H with (x := f) .
+  specialize H0 with (x := e) .
+  destruct H, H0.
+  rewrite <- H0.
   trivial.
 Qed.
 
-(* Jednoznaczność odwrotności  *)
+
+
+(* Jednoznaczność odwrotności - krótszy zapis  *)
 Definition invPro (g : GroupTheo) (e y : Gt g):= idPro g e /\ ( forall(x : Gt g) ,( opt g x y = e /\ opt g y x  = e)).
 
+
 Theorem exOnlyOneInv : forall( g : GroupTheo), forall (e y1 y2 x : Gt g), invPro g e y1 /\ invPro g e y2 -> y1 = y2.
+(* Pomysł dowodu :*)
+(* y1 =  y1 <*> e = y1 <*> (x <*> y2) = (y1 <*> x) <*> y2 = e <*> y2 = y2) *)
 Proof.
   unfold invPro.
   unfold idPro.
   intros.
   destruct H.
+  destruct H, H0.
+  specialize H with (x := y1) .
   destruct H.
+  specialize H0 with (x := y2) .
   destruct H0.
-  pose proof H y1.
-  destruct H3.
-  pose proof H y2.
-  destruct H5.
-  rewrite <- H4.
-  rewrite <- H5.
-  pose proof H1 x.
-  pose proof H2 x.
-  destruct H7.
-  destruct H8.
-  rewrite <- H8 at 1.
-  rewrite <- H9 at 1.
-  rewrite (assoct ).
+  rewrite <- H3.
+  rewrite <- H0.
+  specialize H1 with (x := x).
+  specialize H2 with (x := x).
+  destruct H1, H2.
+  rewrite <- H2 at 1.
+  rewrite <- H5 at 1.
+  rewrite (assoct).
   trivial.
 Qed.
 
@@ -60,6 +65,59 @@ Record Group : Type := group
     e : G;
   
     assoc : forall(x y z : G), op (op x y) z = op x (op y z);
-    id :  forall(x : G), op e x = e /\ op x e = e ;
+    id :  forall(x : G), op e x = x /\ op x e = x ;
     inverse :  forall(x : G), op x (inv x) = e /\ op (inv x) x  = e ;
   }.
+
+
+  
+
+Record AbelianGroup : Type := aGroup 
+  {
+    abelGr : Group;
+    abelComm : forall (x y : G abelGr), op abelGr  x y = op abelGr  y x;
+  }.
+
+
+
+Notation " x <* g *> y" := (op g x y) (at level 50 ).
+
+Theorem pPowerGivesAbelian: forall (g : Group), forall( x y : G g), ( forall (p : G g),  p <* g *>  p = e g ) ->  x <* g *>  y = y  <* g *>  x.
+(*  Idea dowódu
+xy=(yy)xy(xx)=y(yxyx)x=yx.
+ *)
+Proof.
+  intros.
+  specialize H with (p := x) as X.
+  specialize H with (p := y) as Y.
+  pose (id_prop := id g). (* Wprowadzi nam własność id g do założeń *)
+  specialize id_prop with (x := x) as idX.
+  specialize id_prop with (x := y) as idY.
+  destruct idX, idY.
+  rewrite <- H0 at 1.
+  rewrite <- H3 at 1.
+
+  rewrite <- Y at 1.
+  rewrite <- X at 1.
+  (* Wykorzystywanie łączności *)
+  pose (assoc_prop := assoc g).
+  specialize assoc_prop with (x := y) (y := y) (z := x) as assoc_y.
+  rewrite assoc_y.
+  specialize assoc_prop with (x := y) (y := x) (z := x) as assoc_x.
+  rewrite <- assoc_x.
+  specialize assoc_prop with (x := (y <* g *> (y <* g *> x))) (y := (y <* g *> x)) (z := x) as assoc_mul.
+    rewrite <- assoc_mul.
+    specialize assoc_prop with (x := y) (y :=  (y <* g *> x)) (z := (y <* g *> x)) as assoc_doub.
+    rewrite assoc_doub.
+    specialize H with (p := (y <* g *> x)) as YX.
+  rewrite YX at 1.
+  rewrite H3.
+  trivial.
+Qed.
+
+
+
+
+    
+
+
